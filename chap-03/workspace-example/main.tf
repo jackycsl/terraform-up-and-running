b12e986ad13ef828f84aa1d3abf61bc114b5eb7d
Jacky CSL <jackycsl@Jackys-MacBook-Pro-2.local>
2022-10-05 22:14:12 +0800

    chap 3 - remote state on s3 and deploy webserver cluster and database

---
+++ chap-03/workspace-example/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-jackycsl"
+    key    = "workspace-example/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  # instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
+
+}
