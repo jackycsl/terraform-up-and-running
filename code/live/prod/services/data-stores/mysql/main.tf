a9b5b2d621547201a4e14be3b95e474c886fbe01
Jacky CSL <jackycsl@Jackys-MacBook-Pro-2.local>
2022-10-12 15:15:18 +0800

    chap 07 - add multi account, kubernetes local and eks modules and example

---
+++ code/live/prod/services/data-stores/mysql/main.tf
@@ -0,0 +1,57 @@
+terraform {
+
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-jackycsl"
+    key    = "prod/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias  = "primary"
+}
+
+provider "aws" {
+  region = "us-west-1"
+  alias  = "replica"
+}
+
+module "mysql_primary" {
+  source = "../../../../../modules/data-stores/mysql"
+
+  providers = {
+    aws = aws.primary
+  }
+
+  db_name = var.db_name
+
+  db_username = var.db_username
+  db_password = var.db_password
+
+  # Must be enabled to support replication
+  backup_retention_period = 1
+}
+
+module "mysql_replica" {
+  source = "../../../../../modules/data-stores/mysql"
+
+  providers = {
+    aws = aws.replica
+  }
+
+  # Make this a replica of the primary
+  replicate_source_db = module.mysql_primary.arn
+}
