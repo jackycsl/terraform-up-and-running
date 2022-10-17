7423beed02a0c29f66214f9a2afb5bf23299da24
Jacky CSL <jackycsl@Jackys-MacBook-Pro-2.local>
2022-10-17 10:08:49 +0800

    add live-terragrunt example

---
+++ code/live-terragrunt/stage/terragrunt.hcl
@@ -0,0 +1,20 @@
+terragrunt_version_constraint = ">= v0.36.0"
+
+remote_state {
+  backend = "s3"
+
+  generate = {
+    path      = "backend.tf"
+    if_exists = "overwrite"
+  }
+
+  config = {
+
+    bucket         = "terraform-up-and-running-state-jackycsl"
+    key            = "${path_relative_to_include()}/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    dynamodb_table = "terraform-up-and-running-locks"
+
+  }
+}
---
+++ code/live/stage/data-stores/mysql/main.tf
@@ -0,0 +1,37 @@
+terraform {
+
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+
+    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
+    # manually, uncomment and fill in the config below.
+
+    #   bucket = "terraform-up-and-running-state-jackycsl"
+    #   key    = "stage/data-stores/mysql/terraform.tfstate"
+    #   region = "us-east-2"
+
+    #   dynamodb_table = "terraform-up-and-running-locks"
+    #   encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+module "mysql" {
+  source = "../../../../modules/data-stores/mysql"
+
+  db_name     = var.db_name
+  db_username = var.db_username
+  db_password = var.db_password
+}
